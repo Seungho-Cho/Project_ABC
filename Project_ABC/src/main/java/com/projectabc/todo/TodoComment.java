83cb6c641c0e3267ac0d31417384fb3af75fb602
종합관417호 <417@10.10.4.49>
2015-08-31 21:03:58 +0900

    이전 파일 삭제 TodoComment .java .xml 추가

---
+++ Project_ABC/src/main/java/com/projectabc/todo/TodoComment.java
@@ -0,0 +1,40 @@
+package com.projectabc.todo;
+
+public class TodoComment {
+	String commno;
+	String todono;
+	String memid;
+	String comm;
+	String commdate;
+	
+	public String getCommno() {
+		return commno;
+	}
+	public void setCommno(String commno) {
+		this.commno = commno;
+	}
+	public String getTodono() {
+		return todono;
+	}
+	public void setTodono(String todono) {
+		this.todono = todono;
+	}
+	public String getMemid() {
+		return memid;
+	}
+	public void setMemid(String memid) {
+		this.memid = memid;
+	}
+	public String getComm() {
+		return comm;
+	}
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+	public String getCommdate() {
+		return commdate;
+	}
+	public void setCommdate(String commdate) {
+		this.commdate = commdate;
+	}
+}
